39f2d462c1a8e3ca84d38cf4f8ed193e8ab4159e
Alejandro Pobes <73605642+Xabian-hub@users.noreply.github.com>
2024-10-23 15:37:28 +0200

    Todo, y (Ejercicio 1 solo hecho, los demas por hacer)

---
+++ src/main/java/com/alejandropobes/tema04/lib/inputOutput.java
@@ -0,0 +1,29 @@
+package com.alejandropobes.tema04.lib;
+
+import java.util.Scanner;
+
+public class inputOutput {
+    /**
+     * Solicita un texto y valida su longitud  este comprendida entre longitudMinima y longitudMaxima.
+     * @param mensaje Mensaje que se le muestra al usuario.
+     * @param lonitudMinima Numero minimo de caracteres del texto.
+     * @param longitudMaxima numero maximo de caracteres del texto.
+     * @return El texto leido ya validado.
+     */
+
+    public static String solicitarString(String mensaje, int lonitudMinima, int longitudMaxima){
+        Scanner scanner = new Scanner(System.in);
+        String resultado;
+        boolean valido;
+        do {
+            System.out.println(mensaje);
+            resultado = scanner.nextLine();
+            valido = resultado.length() >= lonitudMinima && resultado.length() <= longitudMaxima;
+            if (!valido){
+                System.err.printf("La longitud que has introducido es incorrecta, por favor asegurese de introducir correctamente los datos [%d-%d\n]",lonitudMinima,longitudMaxima);
+            }
+        }while (!valido);
+        scanner.close();
+            return resultado;
+    }
+}
---
+++ src/main/java/com/alejandropobes/tema04/metodos/Ejercicio2.java
@@ -0,0 +1,9 @@
+package com.alejandropobes.tema04.metodos;
+
+import com.alejandropobes.tema04.lib.inputOutput;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        
+    }
+}
